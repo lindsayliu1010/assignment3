90f427d079f30d72356bf346e80301e8b0f124da
lindsayliu <rll46@duke.edu>
2023-09-20 02:02:37 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        arsenal: ["Arsenal", "sans - serif"],
+        imbue: ["Imbue", "serif"],
+        libre: ["Libre Baskerville", "serif"],
+        mukta: ["Mukta", "sans - serif"],
+        marker: ["Permanent Marker", "cursive"],
+        tech: ["Press Start 2P", "cursive"],
+        code: ["Fira Code", "monospace"],
+        cursive: ["Cedarville Cursive", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
